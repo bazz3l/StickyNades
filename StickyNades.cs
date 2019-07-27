6c67c40c594a758b147778afa26e288428cbb9d5
Bazz3l <30571705+bazz3l@users.noreply.github.com>
2019-07-27 14:59:31 +0100

    Update StickyNades.cs

--- StickyNades.cs
+++ StickyNades.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 namespace Oxide.Plugins
 {
-    [Info("Sticky Nades", "Bazz3l", "1.0.0")]
-    [Description("Sticks granades to players")]
+    [Info("Sticky Nades", "Bazz3l", "1.0.1")]
+    [Description("Ability to throw nades at people and stick to them")]
 
     class StickyNades : RustPlugin
     {
@@ -21,13 +21,16 @@ namespace Oxide.Plugins
             if (entity == null) return;
             if (player == null || !permission.UserHasPermission(player.UserIDString, Perm)) return;
             if (entity.ShortPrefabName != "grenade.f1.deployed") return;
+
             RaycastHit hit;
             var raycast = Physics.Raycast(player.eyes.HeadRay(), out hit, 50f);
             BaseEntity rentity = raycast ? hit.GetEntity() : null;
             if (rentity != null && rentity is BasePlayer)
             {
                 entity?.Kill();
-                BaseEntity nade = GameManager.server.CreateEntity(Prefab, rentity.transform.position);
+
+                BaseEntity nade = GameManager.server?.CreateEntity(Prefab, rentity.transform.position);
+                if (nade == null) return;
                 nade.Spawn();
                 nade.SetParent(rentity);
                 nade.transform.localPosition = new Vector3(0.1f, 1f, 0f);
